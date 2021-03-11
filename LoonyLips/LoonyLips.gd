84ec932d7d6a8569a352a9a7f28b75e937d0b026
Sandoche Adittane <sandoche@protonmail.com>
2021-03-11 23:33:30 +0000

    :construction: Added text input

--- LoonyLips/LoonyLips.gd
+++ LoonyLips/LoonyLips.gd
@@ -5,8 +5,17 @@ func _ready():
 	var story = "Once upon a time %s watched %s and thought it was the %s movie since %s"
 	var finalText = story % prompts
 
-	$DisplayText.text = finalText
+	$VBoxContainer/DisplayText.text = finalText
 
 # 	Could also be replaced by this:
 #	var label = get_node("DisplayText")
 #	label.text = story % prompts
+
+
+func _on_PlayerText_text_entered(new_text):
+	update_DisplayText(new_text)
+	
+
+func update_DisplayText(new_text):
+	$VBoxContainer/DisplayText.text = new_text
+	$VBoxContainer/PlayerText.clear()

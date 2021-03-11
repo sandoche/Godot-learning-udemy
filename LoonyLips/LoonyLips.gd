899a1764e7d36be1e977045c5e86a322004f3ff1
Sandoche Adittane <sandoche@protonmail.com>
2021-03-11 23:02:35 +0000

    :construction: Label added

--- LoonyLips/LoonyLips.gd
+++ LoonyLips/LoonyLips.gd
@@ -3,7 +3,10 @@ extends Control
 func _ready():
 	var prompts = ["Yann", "minion", "greatest", "1999"]
 	var story = "Once upon a time %s watched %s and thought it was the %s movie since %s"
-	print(story % prompts)
+	var finalText = story % prompts
 
-	prompts = ["John", "tenet", "worst", "1800"]
-	print(story % prompts)
+	$DisplayText.text = finalText
+
+# 	Could also be replaced by this:
+#	var label = get_node("DisplayText")
+#	label.text = story % prompts

9d91544814c66402ccb60f7b8ee5623ebc398463
Sandoche Adittane <sandoche@protonmail.com>
2021-04-16 22:08:04 +0100

    :sparkles: Many templates are handled

--- LoonyLips/LoonyLips.gd
+++ LoonyLips/LoonyLips.gd
@@ -1,8 +1,21 @@
 extends Control
 
 var player_words = []
-var story = "Once upon a time someone called %s ate a %s flavoured sandwich which made him feel all %s inside. It was a %s day"
-var prompts = ["a name", "a noun", "an adverb", "an adjective"]
+
+var templates = [{
+			"prompts": ["a name", "a noun", "an adverb", "an adjective"],
+			"story": "Once upon a time someone called %s ate a %s flavoured sandwich which made him feel all %s inside. It was a %s day"
+		},
+		{
+			"prompts": ["a name", "a object", "another object"],
+			"story": "Once upon a time %s was trying to build a %s but built instead a %s"
+		},
+		{
+			"prompts": ["an animal", "a food", "an object", "a noun"],
+			"story": "There was a %s that was trying to cook a %s but he accidentally dropped %s on it and it tasted like %s"
+		}]
+
+var current_story
 
 onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
 onready var DisplayText = $VBoxContainer/DisplayText
@@ -10,12 +23,17 @@ onready var DisplayText = $VBoxContainer/DisplayText
 # onready var DisplayText = get_node("DisplayText")
 
 func _ready():
+	set_current_story()
 	DisplayText.text = "Welcome to loony lips! We are going to tell a story and have a wonderful time, "
 	check_player_words_length()
 	PlayerText.grab_focus()
 
 func _on_PlayerText_text_entered(new_text):
 	add_to_player_words()
+
+func set_current_story():
+	randomize()
+	current_story = templates[randi() % templates.size()]
 
 func _on_TextureButton_pressed():
 	if is_story_done():
@@ -31,7 +49,7 @@ func add_to_player_words():
 	check_player_words_length()
 
 func is_story_done():
-	return player_words.size() == prompts.size()
+	return player_words.size() == current_story.prompts.size()
 
 func check_player_words_length():
 	if is_story_done():
@@ -40,10 +58,10 @@ func check_player_words_length():
 		prompt_player()
 
 func tell_story():
-	DisplayText.text = story % player_words
+	DisplayText.text = current_story.story % player_words
 	
 func prompt_player():
-	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
+	DisplayText.text += "May I have " + current_story.prompts[player_words.size()] + " please?"
 
 func end_game():
 	PlayerText.queue_free()

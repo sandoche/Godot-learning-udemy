c48750caaadde826cf760390754399de1236ea85
Sandoche Adittane <sandoche@protonmail.com>
2021-04-18 10:50:11 +0100

    :sparkles: Adding gravity

--- HoppyDays/Player/Player.gd
+++ HoppyDays/Player/Player.gd
@@ -3,8 +3,15 @@ extends KinematicBody2D
 var motion = Vector2(0,0)
 
 const SPEED = 1000
+const GRAVITY = 300
+const UP = Vector2(0,-1)
+const JUMP_SPEED = 3000
 
 func _physics_process(delta):
+	apply_gravity()
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+		
 	if Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed("right") and !Input.is_action_pressed("left"):
@@ -12,4 +19,10 @@ func _physics_process(delta):
 	else:
 		motion.x = 0
 	
-	move_and_slide(motion)
+	move_and_slide(motion, UP)
+
+func apply_gravity():
+	if not is_on_floor():
+		motion.y += GRAVITY
+	else:
+		motion.y = 0

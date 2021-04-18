46119e1872bc5b1f6b62790c839deb648997d0c6
Sandoche Adittane <sandoche@protonmail.com>
2021-04-18 10:53:24 +0100

    :recycle: Refactored code to make it neater

--- HoppyDays/Player/Player.gd
+++ HoppyDays/Player/Player.gd
@@ -8,17 +8,10 @@ const UP = Vector2(0,-1)
 const JUMP_SPEED = 3000
 
 func _physics_process(delta):
+	# every frame apply the following
 	apply_gravity()
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		motion.y -= JUMP_SPEED
-		
-	if Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
-		motion.x = -SPEED
-	elif Input.is_action_pressed("right") and !Input.is_action_pressed("left"):
-		motion.x = SPEED
-	else:
-		motion.x = 0
-	
+	jump()
+	move()
 	move_and_slide(motion, UP)
 
 func apply_gravity():
@@ -26,3 +19,16 @@ func apply_gravity():
 		motion.y += GRAVITY
 	else:
 		motion.y = 0
+
+func jump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+		
+func move():
+	if Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right") and !Input.is_action_pressed("left"):
+		motion.x = SPEED
+	else:
+		motion.x = 0
+	

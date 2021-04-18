21239e58ca30fd02f46a75ce98df0c143336a5c7
Sandoche Adittane <sandoche@protonmail.com>
2021-04-18 10:20:02 +0100

    :sparkles: Character can move left and right

--- HoppyDays/Player/Player.gd
+++ HoppyDays/Player/Player.gd
@@ -1,5 +1,15 @@
 extends KinematicBody2D
 
+var motion = Vector2(0,0)
+
+const SPEED = 1000
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right") and !Input.is_action_pressed("left"):
+		motion.x = SPEED
+	else:
+		motion.x = 0
+	
+	move_and_slide(motion)

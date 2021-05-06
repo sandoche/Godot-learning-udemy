7047a9ca220f2b0cceef714fc73f6213533a9064
Sandoche Adittane <sandoche@protonmail.com>
2021-05-06 19:23:48 +0100

    :construction: Changing jumping and flip of sprites

--- HoppyDays/Player/Player.gd
+++ HoppyDays/Player/Player.gd
@@ -22,7 +22,7 @@ func apply_gravity():
 		motion.y = 0
 
 func jump():
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
 		
 func move():

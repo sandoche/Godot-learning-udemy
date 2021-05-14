098b501a74a0121ed3b3e034ac315d87417bcdc3
Sandoche Adittane <sandoche@protonmail.com>
2021-05-14 19:43:01 +0100

    :construction: Constants values reduced

--- HoppyDays/Player/Player.gd
+++ HoppyDays/Player/Player.gd
@@ -3,9 +3,9 @@ extends KinematicBody2D
 var motion = Vector2(0,0)
 
 const SPEED = 1500
-const GRAVITY = 300
+const GRAVITY = 150
 const UP = Vector2(0,-1)
-const JUMP_SPEED = 5000
+const JUMP_SPEED = 3500
 
 signal animate
 

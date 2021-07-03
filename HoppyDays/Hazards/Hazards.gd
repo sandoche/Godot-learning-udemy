9dffe028735ad718a3355db920a9e82dcb29ecb7
Sandoche Adittane <sandoche@protonmail.com>
2021-07-03 19:06:05 +0200

    :sparkles: Spikeman added

--- HoppyDays/Hazards/Hazards.gd
+++ HoppyDays/Hazards/Hazards.gd
@@ -4,3 +4,7 @@ extends Area2D
 
 func _on_SpikeTop_body_entered(body):
 	get_tree().call_group("Gamestate", "hurt")
+
+
+func _on_Area2D_body_entered(body):
+	get_tree().call_group("Gamestate", "hurt")

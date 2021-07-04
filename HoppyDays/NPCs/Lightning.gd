d83f294961d9363e5422717971b03261f864f60f
Sandoche Adittane <sandoche@protonmail.com>
2021-07-04 18:25:45 +0200

    :sparkles: Lightnings added

--- HoppyDays/NPCs/Lightning.gd
+++ HoppyDays/NPCs/Lightning.gd
@@ -1,5 +1,21 @@
 extends Node2D
 
+const SPEED = 300
+
 func _ready():
 	set_as_toplevel(true)
 	global_position = get_parent().global_position
+	
+func _process(delta):
+	position.y += SPEED * delta
+	manage_collision()
+	
+func manage_collision():
+	var collider = $Area2D.get_overlapping_bodies()
+	for object in collider:
+		if object.name == "Player":
+			get_tree().call_group("Gamestate", "hurt")
+		queue_free()
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()

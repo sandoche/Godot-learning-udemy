c88c6cbd2cb86b6e71685aa2673a922a9aa98aea
Sandoche Adittane <sandoche@protonmail.com>
2021-08-03 13:07:42 +0200

    :construction: Work in progress player

--- HeistMeisters/Characters/TemplateCharacter.gd
+++ HeistMeisters/Characters/TemplateCharacter.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
 
-const SPEED = 10
-const MAX_SPEED = 100
+const SPEED = 20
+const MAX_SPEED = 200
 const FRICTION = 0.1
---
+++ HeistMeisters/Player.gd
@@ -0,0 +1,22 @@
+extends "res://Characters/TemplateCharacter.gd"
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	update_movements()
+	move_and_slide(motion)
+	
+func update_movements():
+	if Input.is_action_pressed("move_down") and not Input.is_action_pressed("move_up"):
+		motion.y = clamp(motion.y + SPEED, 0, MAX_SPEED)
+	elif Input.is_action_pressed("move_up") and not Input.is_action_pressed("move_down"):
+		motion.y = clamp(motion.y - SPEED, -MAX_SPEED, 0)
+	else:
+		motion.y = lerp(motion.y, 0, FRICTION)
+
+	if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
+		motion.x = clamp(motion.x + SPEED, 0, MAX_SPEED)
+	elif Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		motion.x = clamp(motion.x - SPEED, -MAX_SPEED, 0)
+	else:
+		motion.x = lerp(motion.x, 0, FRICTION)

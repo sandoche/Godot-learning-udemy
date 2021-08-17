fc19beb3f6cb1279638f8726d36c378f16e245d8
Sandoche Adittane <sandoche@protonmail.com>
2021-08-17 14:03:32 +0100

    :construction: Work in progress Guard code

--- HeistMeisters/Characters/NPCs/Guard.gd
+++ HeistMeisters/Characters/NPCs/Guard.gd
@@ -1,1 +1,46 @@
 extends "res://Characters/NPCs/PlayerDetection.gd"
+
+onready var navigation = get_tree().get_root().find_node("Navigation2D", true, false)
+onready var destinations = navigation.get_node("Destinations")
+
+var motion
+var possible_destinations
+var path
+
+export var minimum_arrival_distance = 5
+export var walk_speed = 0.5
+
+func _ready():
+	randomize()
+	possible_destinations = destinations.get_children()
+	make_path()
+	
+func _physics_process(delta):
+	navigate()
+
+func navigate():
+	var distance_to_destination = position.distance_to(path[0])
+	if distance_to_destination > minimum_arrival_distance:
+		move()
+	else:
+		update_path()
+
+func move():
+	look_at(path[0])
+	motion = (path[0] - position).normalized() * (MAX_SPEED * walk_speed)
+	move_and_slide(motion)
+
+func update_path():
+	if path.size() == 1 and $Timer.is_stopped():
+		$Timer.start()
+	else:
+		path.remove(0)
+		
+
+func make_path():
+	var new_destination = possible_destinations[randi() % possible_destinations.size() - 1]
+	path = navigation.get_simple_path(position, new_destination.position)
+	print(path)
+
+func _on_Timer_timeout():
+	make_path()

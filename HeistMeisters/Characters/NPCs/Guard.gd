d5d6313cb1673aeafffa594055e182e91be29af6
Sandoche Adittane <sandoche@protonmail.com>
2021-08-17 12:36:48 +0100

    :bug: Fixing bug and connecting camera to playing detection

---
+++ HeistMeisters/Characters/NPCs/Guard.gd
@@ -0,0 +1,1 @@
+extends "res://Characters/NPCs/PlayerDetection.gd"
--- HeistMeisters/Characters/NPCs/PlayerDetection.gd
+++ HeistMeisters/Characters/NPCs/PlayerDetection.gd
@@ -1,16 +1,46 @@
 extends "res://Characters/TemplateCharacter.gd"
 
+# FOV = Field of view
+# LOS = Line of sight
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const FOV_TOLERANCE = 20
+const MAX_DETECTION_RANGE = 640
+const RED = Color(1,0.25,0.25) 
+const WHITE = Color(1,1,1)
 
+var Player
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Search node by name
+	Player = get_node("/root").find_node("Player", true, false)
+	
+func _process(delta):
+	if Player_in_FOV() and Player_in_LOS():
+		$Torch.color = RED
+	else:
+		$Torch.color = WHITE
+		
+func Player_in_FOV():
+	var npc_facing_direction = Vector2(1,0).rotated(global_rotation)
+	var direction_to_Player = (Player.position - global_position).normalized()
+	
+	if abs(direction_to_Player.angle_to(npc_facing_direction)) < deg2rad(FOV_TOLERANCE):
+		return true
+	else:
+		return false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func Player_in_LOS():
+	var space = get_world_2d().direct_space_state
+	var LOS_obstacle = space.intersect_ray(global_position, Player.global_position, [self], collision_mask)
+	
+	if not LOS_obstacle:
+		return false
+	
+	var distance_to_player = Player.global_position.distance_to(global_position)
+	var Player_in_range = distance_to_player < MAX_DETECTION_RANGE
+	
+	if LOS_obstacle.collider == Player && Player_in_range:
+		return true
+	else:
+		return false
+	

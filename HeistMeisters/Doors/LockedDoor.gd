74294948c0abff94609253511e5d5b9beb65691c
Sandoche Adittane <sandoche@protonmail.com>
2021-08-18 18:33:05 +0100

    :sparkles: Adding numpad and locked door

---
+++ HeistMeisters/Doors/LockedDoor.gd
@@ -0,0 +1,10 @@
+extends "res://Doors/Door.gd"
+
+func _on_Door_input_event(viewport, event, shape_idx):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click:
+		$CanvasLayer/Numpad.popup_centered()
+
+func _on_Door_body_exited(body):
+	if body.collision_layer == COLLISION_LAYER_PLAYER:
+		can_click = false
+		$CanvasLayer/Numpad.hide()

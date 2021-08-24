449c4d1a83fe5340b97d8a8e09e5211f1254cd41
Sandoche Adittane <sandoche@protonmail.com>
2021-08-24 16:21:44 +0100

    :sparkles: Hiding / showing stuff depending on the light mode

--- HeistMeisters/Interface/VisionMode.gd
+++ HeistMeisters/Interface/VisionMode.gd
@@ -22,11 +22,15 @@ func DARK_mode():
 	color = DARK
 	$AudioStreamPlayer2D.stream = load("res://SFX/nightvision_off.wav")
 	$AudioStreamPlayer2D.play()
+	get_tree().call_group("lights", "show")
+	get_tree().call_group("labels", "hide")
 
 func NIGHTVISION_mode():
 	color = NIGHTVISION
 	$AudioStreamPlayer2D.stream = load("res://SFX/nightvision.wav")
 	$AudioStreamPlayer2D.play()
+	get_tree().call_group("lights", "hide")
+	get_tree().call_group("labels", "show")
 
 func _on_Timer_timeout():
 	cooldown = false

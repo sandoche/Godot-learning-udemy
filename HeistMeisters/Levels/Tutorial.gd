fb7cdcc33f5265ccb35dcefb6306c78c1e3c6548
Sandoche Adittane <sandoche@protonmail.com>
2021-08-28 13:27:17 +0100

    :sparkles: Adding objective marker

---
+++ HeistMeisters/Levels/Tutorial.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+func _ready():
+	yield(get_tree(), "idle_frame") # to wait until nodes appears
+	update_pointer_position(0)
+
+func update_pointer_position(objective_number):
+	var pointer = $ObjectivePointer
+	var place = $ObjectivePositions.get_child(objective_number)
+	
+	$Tween.interpolate_property(pointer, "position", pointer.position, place.position, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	$Tween.start()
+	$MessageSound.play()
+
+func _on_MoveObjective_body_entered(body):
+	update_pointer_position(1)
+
+
+func _on_DoorObjective_body_entered(body):
+	update_pointer_position(2)
+
+
+func _on_NightvisionObjective_body_entered(body):
+	update_pointer_position(3)
+
+
+func _on_BriefcaseObjective_body_entered(body):
+	update_pointer_position(4)

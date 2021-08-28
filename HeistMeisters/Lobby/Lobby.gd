3e8b413058c3e4498d9ed15dfe22a639d37c3d09
Sandoche Adittane <sandoche@protonmail.com>
2021-08-28 13:03:41 +0100

    :construction: Adding objectives to the tutorial

--- HeistMeisters/Lobby/Lobby.gd
+++ HeistMeisters/Lobby/Lobby.gd
@@ -3,5 +3,8 @@ extends Control
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://Levels/Level1.tscn")
 
+func _on_TutorialButton_pressed():
+	get_tree().change_scene("res://Levels/Tutorial.tscn")
+
 func _on_QuitButton_pressed():
 	get_tree().quit()

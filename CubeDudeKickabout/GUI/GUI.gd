cdbceca88fecc8fd98ab6eda825566b87f881d13
Sandoche Adittane <sandoche@protonmail.com>
2021-09-01 17:45:42 +0100

    :sparkles: Adding the winning popup

--- CubeDudeKickabout/GUI/GUI.gd
+++ CubeDudeKickabout/GUI/GUI.gd
@@ -3,3 +3,13 @@ extends CanvasLayer
 func update_score(player, score):
 	var score_label = get_node("Banner/HBoxContainer/Player%sScore" % player)
 	score_label.text = str(score)
+
+
+func _on_Button_pressed():
+	get_tree().reload_current_scene()
+
+func game_over(player):
+	var label = $Popup/NinePatchRect/VBoxContainer/Label
+	label.text = "Player " + str(player) + " wins!"
+	$Popup.popup_centered()
+	
--- CubeDudeKickabout/Level/Level.gd
+++ CubeDudeKickabout/Level/Level.gd
@@ -3,6 +3,8 @@ extends Spatial
 var Player1_score = 0
 var Player2_score = 0
 
+export var target_score = 3
+
 func _on_GoalDetector_body_entered(body, goal_id):
 	get_tree().call_group("game_pieces", "freeze")
 	$Timer.start()
@@ -23,4 +25,9 @@ func update_score(player):
 			new_score = Player2_score
 			
 	$GUI.update_score(player, new_score)
+	check_game_over(player, new_score)
 
+func check_game_over(player, score):
+	if score == target_score:
+		$Timer.queue_free()
+		$GUI.game_over(player)

82f72c2abe50b1217f979d3abadda476c78ab234
Sandoche Adittane <sandoche@protonmail.com>
2021-09-16 14:39:19 +0100

    :sparkles: Adding camera controled by mouse

--- FoodFight/Scenes/Characters/Players/Player.gd
+++ FoodFight/Scenes/Characters/Players/Player.gd
@@ -6,13 +6,23 @@ const UP = Vector3(0,1,0)
 var motion = Vector3()
 var movement_state = 0 # idle is 0, run is 1
 
+var mouse_sensitivity = 500
+
 const MIN_BLEND_SPEED = 0.125 # minimum movement threshold before we start the animation
 const BLEND_TO_RUN = 0.075
 const BLEND_TO_IDLE = 0.1
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _physics_process(delta):
 	move()
 	animate()
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation = h_camera_rotation(-event.relative.x/mouse_sensitivity)
+		$Camera.rotation = v_camera_rotation(-event.relative.y/mouse_sensitivity)
 	
 func move():
 	var x = Input.get_action_strength("forward") - Input.get_action_strength("back")
@@ -38,3 +48,10 @@ func animate():
 	
 	animation["parameters/Move/blend_amount"] = movement_state
 
+func h_camera_rotation(camera_rotation):
+	return rotation + Vector3(0, camera_rotation, 0) 
+	
+func v_camera_rotation(camera_rotation):
+	var rotation = $Camera.rotation + Vector3(camera_rotation, 0, 0) 
+	rotation.x = clamp(rotation.x, PI/-8, PI/8)
+	return rotation

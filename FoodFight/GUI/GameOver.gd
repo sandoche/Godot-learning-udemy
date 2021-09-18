2ba29cbd7bff0c147ea48ea092c95aa6c11d0a86
Sandoche Adittane <sandoche@protonmail.com>
2021-09-18 22:48:20 +0100

    :sparkles: Gameover screen added

--- FoodFight/GUI/GameOver.gd
+++ FoodFight/GUI/GameOver.gd
@@ -1,7 +1,10 @@
 extends Control
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func _on_RestartButton_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://Level/Level1.tscn")
 
 func _on_QuitButton_pressed():
 	get_tree().quit()

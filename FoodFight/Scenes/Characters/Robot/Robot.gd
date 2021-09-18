1f69ff2782777ca0a038391717ca4ecfd094888b
Sandoche Adittane <sandoche@protonmail.com>
2021-09-18 12:26:37 +0100

    :sparkles: Adding 2 animated robots

--- FoodFight/Scenes/Characters/Robot/Robot.gd
+++ FoodFight/Scenes/Characters/Robot/Robot.gd
@@ -4,6 +4,4 @@ var Player = preload("res://Scenes/Characters/Players/Player.gd")
 
 func _process(delta):
 	if $RayCast.is_colliding():
-		var body = $RayCast.get_collider()
-		if body is Player:
-			try_to_fire()
+		try_to_fire()
